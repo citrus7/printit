5e23db6d93f081f8e3b47b4287cfc04a73df1b92
Jon Sorrells <jsorrells@onshape.com>
2016-04-10 04:29:01 -0400

    object properties

--- app/scripts/services/dataService.js
+++ app/scripts/services/dataService.js
@@ -89,12 +89,18 @@
 				//initialize toolObjects
 				var object3 = {};
 				object3.name = "Screwdriver";
-				object3.cooldown = 0;
+                               object3.plastic = 30;
+                               object3.steel = 10;
+                               object3.gold = 0;
+                               object3.time = 30;
                                object3.id = 3;
 
 				var object4 = {};
 				object4.name = "Pulley";
-				object4.cooldown = 0;
+                               object4.plastic = 50;
+                               object4.steel = 0;
+                               object4.gold = 0;
+                               object4.time = 50;
                                object4.id = 5;
 
 				toolObjects.push(object3);
@@ -103,12 +109,18 @@
 				//initialize partObjects
 				var object5 = {};
 				object5.name = "Screw";
-				object5.cooldown = 0;
+                               object2.plastic = 0;
+                               object2.steel = 10;
+                               object2.gold = 0;
+                               object2.time = 40;
                                object5.id = 2;
 
 				var object6 = {};
 				object6.name = "Box";
-				object6.cooldown = 0;
+                               object2.plastic = 30;
+                               object2.steel = 0;
+                               object2.gold = 0;
+                               object2.time = 20;
                                object6.id = 0;
 
 				partObjects.push(object5);
